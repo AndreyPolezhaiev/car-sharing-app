579e3d5aa16f92f503e28955a88361b0a5c3d959
Andrey Polezhaiev <workpolezhaiev@gmail.com>
2024-03-01 13:34:41 +0200

    add checkstyle and dependencies

---
+++ src/test/java/com/polezhaiev/carsharingapp/CarSharingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.polezhaiev.carsharingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = CarSharingAppApplicationTests.class)
+class CarSharingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
